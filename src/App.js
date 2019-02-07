572352fcda483da2d73fdd301594347bf5b3c38b
James Martineau <jamesmartineau@Jamess-MacBook-Air.local>
2019-02-06 19:10:28 -0500

    inital create-react-app struture

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Portfolio page under construction...stay tuned folks
+          </p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
