9f30f91177885d66c22ef29114b64a8b3b82112f
James Martineau <jamesmartineau@Jamess-MacBook-Air.local>
2019-02-08 11:15:02 -0500

    added styling and restructuring of app and portfolio containers

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,31 +1,15 @@
 import React, { Component } from 'react';
-import { Container, Row } from 'react-materialize';
 import Nav from '../components/Nav';
 import Banner from '../components/Banner';
-import ProjectCard from '../components/ProjectCard';
-import * as projectData from '../helpers/projects.json';
+import Portfolio from './Portfolio';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="app-container">
         <Nav/>
         <Banner />
-
-          <Row>
-            { projectData.projects.map(project => {
-              return (
-                <ProjectCard 
-                  title={project.title}
-                  description={project.description}
-                  appLink={project.appLink}
-                  repoLink={project.repoLink}
-                  techStack={project.techStack}
-                />
-              )
-            })}
-          </Row>
-
+        <Portfolio />
       </div>
     );
   }
---
+++ src/containers/Portfolio.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { Col, Row } from 'react-materialize';
+import ProjectCard from '../components/ProjectCard';
+import * as projectData from '../helpers/projects.json';
+
+class Portfolio extends Component {
+  render() {
+    return (
+      <div className="portfolio-container">
+          <Row>
+            <Col s={12} className='center'>
+              <div className='portfolio-sub-text'>
+                Full stack developer by day, passionate coder in life.
+              </div>
+            </Col>
+          </Row>
+          <Row>
+            { projectData.projects.map(project => {
+              return (
+                <ProjectCard 
+                  title={project.title}
+                  description={project.description}
+                  appLink={project.appLink}
+                  repoLink={project.repoLink}
+                  techStack={project.techStack}
+                />
+              )
+            })}
+          </Row>
+      </div>
+    );
+  }
+}
+
+export default Portfolio;
