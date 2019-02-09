fd1c1cd57160b3b6ee75d05ba782b0cfcbfe4c5f
James Martineau <jamesmartineau@Jamess-MacBook-Air.local>
2019-02-08 19:09:14 -0500

    added loading spinner to optimize image loading

---
+++ src/components/LoadingSpinner.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import Loader from 'react-loader-spinner';
+import { Row, Col } from 'react-materialize';
+import Profile from '../images/profile.jpg';
+import Family from '../images/family_photo.jpg';
+
+export class LoadingSpinner extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            countDown: 3
+        }
+    }
+
+    componentDidMount(){
+        let counter = 3;
+        let x = setInterval(() => {
+            counter--;
+            if(counter >0 ){
+                this.setState({countDown: counter});
+            } else {
+                clearInterval(x);
+                this.props.handleStopLoader();
+            }
+        }, 1000);
+    }
+
+    render(){
+        return(
+            <div className='loading-container center'>
+                <Row>
+                    <Col s={12}>
+                        <Loader
+                            type="Plane"
+                            color="#db697a"
+                            height="100"	
+                            width="100"
+                        />
+                    </Col>
+
+                    <Col s={12}>
+                        <div className='loading-text'>
+                            <h5>Look into the plane...you're getting sleepy!</h5>
+                            <p>You will wake in: {this.state.countDown}</p>
+                        </div>
+                        <div className='image' style={ {backgroundImage: `url(${Profile})`} } />
+                        <div className='image' style={ {backgroundImage: `url(${Family})`} } />
+                    </Col>
+                </Row>
+            </div>
+        )
+    }
+}
+
+export default LoadingSpinner;
--- src/containers/App.js
+++ src/containers/App.js
@@ -4,18 +4,37 @@ import Nav from '../components/Nav';
 import Portfolio from './Portfolio';
 import AboutMe from './AboutMe';
 import NoMatch from '../components/NoMatch';
+import LoadingSpinner from '../components/LoadingSpinner';
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.handleStopLoader = this.handleStopLoader.bind(this);
+    this.state = {
+      loading: true,
+      firstTimeLoad: true
+    }
+  }
+
+  handleStopLoader(){
+    this.setState({loading: false})
+  }
+
   render() {
     return (
       <BrowserRouter>
         <div className="app-container">
           <Nav/>
+          { this.state.loading ? (
+            <LoadingSpinner handleStopLoader={this.handleStopLoader}/>
+          ) : (
           <Switch>
             <Route exact path='/' component={Portfolio} />
             <Route path='/aboutme' component={AboutMe} />
             <Route component={NoMatch} />
           </Switch>
+          )}
         </div>
       </BrowserRouter>
     );
