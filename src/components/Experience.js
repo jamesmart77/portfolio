f8cba7aceb4ed716fa4da56c4691f9fd9f8f7132
James Martineau <jamesmartineau@Jamess-MacBook-Air.local>
2019-02-13 14:58:35 -0500

    styling updates in progress

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -8,18 +8,18 @@ export class Experience extends Component {
             <div className='experience-container'>
                 <Row>
                     <Row className='experience-container-row'>
-                        <Col m={4} s={12} className='experience-container-row-col'>
+                        <Col l={4} m={12} s={12} className='experience-container-row-col'>
                             <div className='exp-section'>
                                 <h4 className='exp-section-header center truncate'>Front-End</h4>
                                 <Row>
                                     <div className='exp-section-sub-header'>Experienced with modern frameworks, styling, testing and more!</div>
                                 </Row>
-                                <Row>
+                                <Row className='exp-section-sub-row'>
                                     {experience.frontEnd.map(item => {
                                         return (
                                             <Chip 
                                                 key={item.name} 
-                                                className='experience-chip'>
+                                                className='experience-chip truncate'>
                                                     {item.img && <img src={item.img} alt={item.name}/>}
                                                     {item.name}
                                             </Chip>
@@ -28,18 +28,18 @@ export class Experience extends Component {
                                 </Row>
                             </div>
                         </Col>
-                        <Col m={4} s={12} className='experience-container-row-col'>
+                        <Col l={4} m={12} s={12} className='experience-container-row-col'>
                             <div className='exp-section'>
                                 <h4 className='exp-section-header center truncate'>Back-End</h4>
                                 <Row>
                                     <div className='exp-section-sub-header'>Multiple runtime environments, web frameworks, database design, and data protection.</div>
                                 </Row>
-                                <Row>
+                                <Row className='exp-section-sub-row'>
                                     {experience.backEnd.map(item => {
                                         return (
                                             <Chip 
                                                 key={item.name} 
-                                                className='experience-chip'>
+                                                className='experience-chip truncate'>
                                                     {item.img && <img src={item.img} alt={item.name}/>}
                                                     {item.name}
                                             </Chip>
@@ -48,17 +48,17 @@ export class Experience extends Component {
                                 </Row>
                             </div>
                         </Col>
-                        <Col m={4} s={12} className='experience-container-row-col'>
+                        <Col l={4} m={12} s={12} className='experience-container-row-col'>
                             <div className='exp-section'>
                                 <h4 className='exp-section-header center truncate'>Competencies</h4>
                                 <Row>
                                     <div className='exp-section-sub-header'>From strong soft-skills to high technical aptitude, I provide a balanced development approach.</div>
                                 </Row>
-                                <Row>
+                                <Row className='exp-section-sub-row'>
                                     {experience.competencies.map(item => {
                                         return (
                                             <Chip key={item.name} 
-                                                    className='experience-chip'>{item.name}</Chip>
+                                                    className='experience-chip truncate'>{item.name}</Chip>
                                         )
                                     })}
                                 </Row>
