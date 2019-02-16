f5d823e39b230cd7457e4d97f60cff1b3194e8f2
James Martineau <jamesmartineau@Jamess-MacBook-Air.local>
2019-02-16 14:04:44 -0500

    Updates

---
+++ precache-manifest.94cc73cb725692f3d6a51b5a6510d906.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "ccc931c106071dbec2f81a6c45c0ead8",
+    "url": "/static/media/profile.ccc931c1.jpg"
+  },
+  {
+    "revision": "89fee78c70d9cdaac31f",
+    "url": "/static/css/main.078bcd69.chunk.css"
+  },
+  {
+    "revision": "37c5bd3de77ee00087c30e909777686c",
+    "url": "/static/media/hangman.37c5bd3d.gif"
+  },
+  {
+    "revision": "12262e0e0b299568109a",
+    "url": "/static/js/1.12262e0e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "89fee78c70d9cdaac31f",
+    "url": "/static/js/main.89fee78c.chunk.js"
+  },
+  {
+    "revision": "7097c0f824f4df0dfbaddd62e9e83fdf",
+    "url": "/static/media/family_photo.7097c0f8.jpg"
+  },
+  {
+    "revision": "0e27df3d07601008c32b833fba751de1",
+    "url": "/static/media/wood_working.0e27df3d.jpg"
+  },
+  {
+    "revision": "e96e5ebab16160fa7035b45ba8d22681",
+    "url": "/static/media/code.e96e5eba.jpg"
+  },
+  {
+    "revision": "12262e0e0b299568109a",
+    "url": "/static/css/1.053cc2e8.chunk.css"
+  },
+  {
+    "revision": "639734fdbdbd1febdadfb6a1147f0fc2",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.29c4f1a750818610e834aff0e6f78643.js"
+  "/precache-manifest.94cc73cb725692f3d6a51b5a6510d906.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/portfolio/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
